67851215b087cbb39c4ada9fe43dcb2615e42d84
Karthikeyen K <114602409+karthikeyen78@users.noreply.github.com>
2024-03-02 23:28:44 +0530

    Update contract0.sol

--- contract0.sol
+++ contract0.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BasicContract {
+    // State variable to store an integer value
+    uint256 public myNumber;
+
+    // Event to log when the value is updated
+    event ValueUpdated(uint256 newValue);
+
+    // Constructor to initialize the state variable
+    constructor(uint256 _initialValue) {
+        myNumber = _initialValue;
+    }
+
+    // Function to update the state variable
+    function setNumber(uint256 _newNumber) public {
+        myNumber = _newNumber;
+        emit ValueUpdated(_newNumber);
+    }
+
+    // Function to retrieve the current value
+    function getNumber() public view returns (uint256) {
+        return myNumber;
+    }
+}
 
